c36c42203396d52cf8112a5721efa5af4d178c3e
vanceleon <vanceleon44@gmail.com>
2020-01-30 18:53:09 -0500

    first commit

---
+++ practice_vanilla_server/server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+
+const todos = [
+  { id: 1, text: 'Todo 1' },
+  { id: 2, text: 'Todo 2' },
+  { id: 3, text: 'Todo 3' }
+];
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+  let body = [];
+  req
+    .on('data', chunk => {
+      body.push(chunk);
+    })
+    .on('end', () => {
+      body = Buffer.concat(body).toString();
+
+      let status = 404;
+      const response = {
+        success: false,
+        data: null
+      };
+      if (method === 'GET' && url === '/todos') {
+        status = 200;
+        response.success = true;
+        response.data = todos;
+      } else if (method === 'POST' && url === '/todos') {
+        const { id, text } = JSON.parse(body);
+        if (!id || !text) {
+          status = 400;
+          response.error = 'Please add id and text';
+        } else {
+          todos.push({ id, text });
+          status = 201;
+          response.success = true;
+          response.data = todos;
+        }
+      }
+      res.writeHead(status, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+      });
+      res.end(JSON.stringify(response));
+    });
+});
+// console.log(req.headers.authorization);
+
+const PORT = 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
